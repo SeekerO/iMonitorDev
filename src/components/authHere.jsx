dfd367b625f58bd1a24637fb2fb3ccf0697ecd81
Seeker <baisa.156269@stamaria.sti.edu.ph>
2023-12-19 08:43:48 +0800

    com

--- src/components/authHere.jsx
+++ src/components/authHere.jsx
@@ -2,9 +2,7 @@
 export const msalConfig = {
   auth: {
     clientId: "0ba355cc-d023-478b-84dd-5e716f734b64",
-    // clientId: "0ba355cc-d023-478b-84dd-5e716f734b64",
     authority: "https://login.microsoftonline.com/stamaria.sti.edu.ph",
-    redirectUri: "http://localhost:3000/iMonitorDev",
   },
   cache: {
     cacheLocation: "sessionStorage", // or 'localStorage'
