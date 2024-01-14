ae89da4367cbf9249ce3adf031b4b192f3de35fa
Seeker <baisa.156269@stamaria.sti.edu.ph>
2024-01-14 13:54:20 +0800

    comm

--- src/components/authHere.jsx
+++ src/components/authHere.jsx
@@ -4,7 +4,6 @@ export const msalConfig = {
   auth: {
     clientId: Id,
     authority: "https://login.microsoftonline.com/stamaria.sti.edu.ph",
-    redirectUri: "/",
   },
   cache: {
     cacheLocation: "localStorage",
